8f23080689d43e67b902706ef2a88472c3b72c4f
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-11-27 17:04:32 +0530

    27nov17:04

--- js/page.js
+++ js/page.js
@@ -1,7 +1,15 @@
 let ctx=document.getElementById('monthlySales').getContext('2d');
 let piCtx=document.getElementById('deptSales').getContext('2d');
-let monthlySales=Array.of(12,9,3);
-let monthlyLabels=Array.of('Oct','Nov','Dec');
+let yearlyLabel=document.getElementById('yearlyTotal');
+let monthlySales=Array.of(12000000,7700230,9000000,12454540);
+let monthlyLabels=Array.of('Jan-Mar','Apr-Jun','Jul-Sep','Oct-Dec');
+let deptSales=Array.of(12,9,3);
+let deptLabels=Array.of('Hiking','Running','Hunting');
+function addYealyTotal(a,b,c,d){
+    return a+b+c;
+}
+let yearlyTotal=addYealyTotal(...monthlySales);
+yearlyLabel.innerHTML="&#x20B9;" +yearlyTotal;
 var monthlySalesChart=new Chart(ctx,{
     type:'bar',
     data:{labels :
@@ -11,15 +19,16 @@ var monthlySalesChart=new Chart(ctx,{
             data:monthlySales,
             backgroundColor:["rgba(238,184,104,1)",
         "rgba(75,166,223,1)",
-    "rgba(239,118,122,1)",],
+    "rgba(239,118,122,1)",
+    "rgba(10,75,100,1)",],
     borderWidth:0,
 }]
 },
 options:{
     scales:{
-        yAxis:[{
+        yAxes:[{
             ticks:{
-                beginAtZero:false,
+                beginAtZero:true,
             }
         }]
     }}
@@ -27,10 +36,10 @@ options:{
 var deptSalesChart=new Chart(piCtx,{
     type:'pie',
     data:{labels :
-        ['Hiking','Running','Hunting'],
+        deptLabels,
         datasets:[{
             label:'# of Sales',
-            data:[12,19,3],
+            data:deptSales,
             backgroundColor:["rgba(238,184,104,1)",
         "rgba(75,166,223,1)",
     "rgba(239,118,122,1)",],
